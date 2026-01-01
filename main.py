a2e745cafef8443d475225013051432c5bad4073
Shabnum Yousuf <38584364+shabnum99@users.noreply.github.com>
2026-01-01 06:45:18 +0000

    Day 2: Fast API with CRUD

--- main.py
+++ main.py
@@ -1,28 +1,73 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+notes = []
 
-@app.get("/hello")
-def say_hello(name: str = "Stranger"):
-    return {"message": f"Hello, {name}!"}
+note_id_counter = 1
 
-@app.get("/double")
-def double_number(number: int = 1):
-    doubled = number * 2
-    return({"original" : number, "doubled" : doubled})
+class noteCreate(BaseModel):
+    title: str
+    content: str
 
-@app.get("/add")
-def add(number1: int = 1, number2: int = 1):
-    added_num = number1 + number2
-    return {"Sum" : added_num}
+@app.post("/notes")
+def create_note(note: noteCreate):
+    global note_id_counter
 
-@app.get("/greet-age")
-def greet(name: str = "NAME", age: int = 1):
-    if(age>=18):
-        return {"message" : f"Hello {name}, you are an adult"}
-    else:
-        return {"message" : f"Hello {name}, you are a minor"}
+    new_note = {
+        "id": note_id_counter,
+        "title": note.title,
+        "content": note.content
+    }
+
+    notes.append(new_note)
+    note_id_counter = note_id_counter + 1
+
+    return {
+        "message" : "Note Created Successfully",
+        "note" : new_note
+    }
+
+@app.get("/notes")
+def get_notes():
+    return {
+        "count": len(notes),
+        "notes": notes
+    }
+
+@app.get("/notes/{note_id}")
+def get_note(note_id: int):
+    for note in notes:
+        if(note["id"]== note_id):
+            return note
+    return {"error": "No Note Found!"}
+
+
+@app.delete("/notes/{note_id}")
+def delete_notes(note_id: int):
+    for index, note in enumerate(notes):
+        if(note["id"] == note_id):
+            deleted_note = notes.pop(index)
+            return {
+                "message": "Note Deleted Successfully!",
+                "Deleted Node": deleted_note
+            }
+    return {"error": "Node Not Found!"}
+
+class nodeUpdate(BaseModel):
+    title: str
+    content: str
+
+@app.put("/notes/{note_id}")
+def update_note(note_id: int, updated_note: nodeUpdate):
+    for note in notes:
+        if(note["id"] == note_id):
+            note["title"] = update_note.title
+            note["content"] = update_note.content
+            return {
+                "message": "Note updated Successfully",
+                "Updated Note": note
+            }
+    return {"error": "No Note Found"}
